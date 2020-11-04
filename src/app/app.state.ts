3681a0d04bb6e26771c0cd1854e3c1781134bcb0
KobusKuch <kobus.de.koning@gmail.com>
2020-11-04 12:07:01 +0100

    initial appstate added

---
+++ src/app/app.state.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { State, Selector, Action, StateContext } from '@ngxs/store';
+//import { Navigate } from '@ngxs/router-plugin';
+//import { tap } from 'rxjs/operators';
+
+export interface AppStateModel {
+  unsavedChanges: boolean;
+}
+
+@State<AppStateModel>({
+  name: 'app',
+  defaults: {
+    unsavedChanges: false,
+  }
+})
+@Injectable()
+export class AppState {
+
+  constructor(
+  ) {}
+
+  }
+}
